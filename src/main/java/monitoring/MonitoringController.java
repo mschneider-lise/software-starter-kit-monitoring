8d8fb2c5ed2c912edeb2759b90286b666f7facf4
Matthias Schneider <matthias.schneider@lise.de>
2025-06-05 13:19:51 +0200

    Rename Controller and remove content

---
+++ src/main/java/monitoring/MonitoringController.java
@@ -0,0 +1,9 @@
+package monitoring;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MonitoringController {
+    public MonitoringController() {
+    }
+}
